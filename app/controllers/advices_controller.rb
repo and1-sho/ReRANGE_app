fd2dfc0456653e12f43b99c7a8b376e05c108781
稲永翔 <shoma@def.local>
2026-04-01 21:09:16 +0900

    coachのみ投稿可能なアドバイス作成を実装

--- app/controllers/advices_controller.rb
+++ app/controllers/advices_controller.rb
@@ -1,7 +1,35 @@
 class AdvicesController < ApplicationController
+  # ログインしていない人はアクセスできない設定
+  before_action :authenticate_user!
+  # coachかをチェックする設定
+  before_action :ensure_coach!, only: [:new, :create]
+
+
   def new
+    @request = Request.find(params[:request_id])
+    @advice = Advice.new
   end
 
   def create
+    @request = Request.find(params[:request_id])
+    @advice = @request.build_advice(advice_params)
+    @advice.user = current_user
+
+    if @advice.save
+      redirect_to request_path(@request), notice: "アドバイスを投稿しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def advice_params
+    params.require(:advice).permit(:body)
+  end
+
+  def ensure_coach!
+    redirect_to requests_path, alert: "コーチのみアドバイスできます" unless current_user.coach?
   end
 end
